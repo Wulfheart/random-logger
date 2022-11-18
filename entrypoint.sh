2986c600d88185dcfd5578bdc00814318b9f1ca1
Alex Wulf <aw@dc.ag>
2022-11-18 11:14:14 +0100

    Reversing

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ do
    WAIT=$(shuf -i $1-$2 -n 1)
    sleep $(echo "scale=4; $WAIT/1000" | bc)
    I=$(shuf -i 1-4 -n 1)
-   D=`date -Ins`
+   D=`date -Iseconds`
    case "$I" in
       "1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
       ;;
